f44a7d0013c4f83892a159edff5aff77c664d255
Maxence_Sanchez <maxence.sanchez-varas-leclercq@uha.fr>
2025-02-27 16:58:12 +0100

    cxw

--- vues/reglement.php
+++ vues/reglement.php
@@ -4,7 +4,7 @@ $styles = "styles/style_reglement.css";
 
 $librairie = '';
 
-$script = "";
+$script = "<script src='js/reglement.js'></script>";
 
 $resultpanier = "";
 $total = 0;
@@ -126,7 +126,7 @@ if (empty($panier) && empty($souvenirs)) {
                         </div>
                         <div class="line2">
                             <div class="puce">
-
+                                <img src="" alt="puce en or">
                             </div>
                         </div>
                         <div class="numbers">
@@ -166,59 +166,7 @@ if (empty($panier) && empty($souvenirs)) {
                 </div>
                 <button>Valider la commande</button>
             </div>
-
             <?= $erreur ?>
         </form>
     </div>
 </div>
-
-
-
-
-<script>
-    document.querySelector('body').addEventListener('keyup', testInput)
-    let num = document.querySelector('#num')
-    function testInput(event) {
-        let touche = event.key;
-        if (num.value == "4") {
-            document.querySelector('.carte_front').classList.add('visa')
-            document.querySelector('.carte_front').classList.remove('mastercard')
-            document.querySelector('.carte_back').classList.add('visa')
-            document.querySelector('.carte_back').classList.remove('mastercard')
-            document.querySelector('#logo').src = '../img/Visa-logo.png'
-        }
-        if (num.value == "51" || num.value == "55") {
-            document.querySelector('.carte_front').classList.add('mastercard')
-            document.querySelector('.carte_front').classList.remove('visa')
-            document.querySelector('.carte_back').classList.remove('visa')
-            document.querySelector('.carte_back').classList.add('mastercard')
-            document.querySelector('#logo').src = '../img/Mastercard-logo.svg';
-        }
-        if (num.value == "") {
-            document.querySelector('.carte_front').classList.remove('visa')
-            document.querySelector('.carte_front').classList.remove('mastercard')
-            document.querySelector('.carte_back').classList.remove('visa')
-            document.querySelector('.carte_back').classList.remove('mastercard')
-            document.querySelector('#logo').src = '../img/cb.jpg';
-        }
-    }
-    num.addEventListener('keypress', testerEspace)
-    function testerEspace(event) {
-        if (!isNaN(event.key) || event.key == "") {
-            console.log('ok')
-        }
-        else {
-            event.preventDefault()
-        }
-    }
-    num.addEventListener('change', changementinput)
-    function changementinput() {
-        let test = num.value;
-
-        let resultat = test.replace(/ /g, "")
-
-        num.value = resultat;
-
-        num.value = resultat.replace(/(.{4})/g, "$1 ");
-    }
-</script>

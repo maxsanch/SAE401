494b6d2a7512369bd76b5d380921d69bdfedc47b
Alexandre Spitzer <spitzeralexandre@gmail.com>
2025-02-27 08:10:55 +0100

    avancement shop

--- vues/shop.php
+++ vues/shop.php
@@ -1,6 +1,6 @@
 <?php
 
-$styles = "../styles/style_souvenrirs.css";
+$styles = '../styles/style_souvenirs.css';
 
 $result = "";
 
@@ -16,9 +16,9 @@ foreach ($objets as $valeur) {
     }
 
     if ($valeur['stock'] == 0) {
-        $result .= "<div class='case'><img src='" . $phototest . "'><div class='infotop><div class='titre'>" . $valeur['nom'] . "</div><div class='prix'>" . $valeur['prix'] . " €</div></div><div class='description'>description : " . $valeur['description'] . "</div><div class='disponibilite'><div class='stock'>indisponible : " . $valeur['stock'] . "</div></div><form method='post' action='#'><input type='number' name='quantite' value='0' min='0' max='" . $valeur['stock'] . "' placeholder='quantite'><button>Ajouter au panier</button></form></div>";
+        $result .= "<div class='case'><img src='" . $phototest . "'><div class='infotop'><div class='titre'>" . $valeur['nom'] . "</div><div class='prix'>" . $valeur['prix'] . " €</div></div><div class='description'>description : " . $valeur['description'] . "</div><div class='disponibilite'><div class='stock'>indisponible : " . $valeur['stock'] . "</div></div><form method='post' action='#'><input type='number' name='quantite' value='0' min='0' max='" . $valeur['stock'] . "' placeholder='quantite'><button>Ajouter au panier</button></form></div>";
     } else {
-        $result .= "<div class='case'><img src='" . $phototest . "'><div class='infotop><div class='titre'>" . $valeur['nom'] . "</div><div class='prix'>" . $valeur['prix'] . " €</div></div><div class='description'>description : " . $valeur['description'] . "</div><div class='disponibilite'><div class='stock'>disponible : " . $valeur['stock'] . "</div></div><form method='post' action='index.php?page=ajouterObjPanier&idobj=" . $valeur['id_objet_shop'] . "''><input type='number' name='quantite' value='1' min='0' max='" . $valeur['stock'] . "' placeholder='quantite'><button>Ajouter au panier</button></form></div>";
+        $result .= "<div class='case'><img src='" . $phototest . "'><div class='infotop'><div class='titre'>" . $valeur['nom'] . "</div><div class='prix'>" . $valeur['prix'] . " €</div></div><div class='description'>description : " . $valeur['description'] . "</div><div class='disponibilite'><div class='stock'>disponible : " . $valeur['stock'] . "</div></div><form method='post' action='index.php?page=ajouterObjPanier&idobj=" . $valeur['id_objet_shop'] . "''><input type='number' name='quantite' value='1' min='0' max='" . $valeur['stock'] . "' placeholder='quantite'><button>Ajouter au panier</button></form></div>";
     }
 }
 

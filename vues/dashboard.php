1c60445e96512e8866742bbb3990c8bcb44e6200
Temecoo <goguet.noah@gmail.com>
2025-02-28 09:50:57 +0100

    f
    f

--- vues/dashboard.php
+++ vues/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 require_once "controleur/ctlPanier.php";
 
-$styles = "";
+$styles = "../styles/style_dashboard.css";
 
 $librairie = '';
 
@@ -26,8 +26,8 @@ if (count($users)) {
         $paniervalid = $acces->getValidPaniers($ligne['Id_utilisateur']);
         $result .= "<div class='GrandeCase'><div class='PetiteCase'><a href='index.php?page=PhotoUser&idUser=" . $ligne['Id_utilisateur'] . "'><img class='photo' style='height: 250px; object-fit: cover;'' src='../" . $phototest . "' alt='photo'></a><b>" . $ligne['prenom'] . "</b><div>Dernière connexion : " . $ligne['connexion'] . "</div><div>paniers validés : " . count($paniervalid) . "</div><a class='Information' href='index.php?page=informationsUser&idUser=" . $ligne['Id_utilisateur'] . "'>Information</a></div></div>";
     }
-} else{
-     $result .= "<div class='reponse'>Aucun Utilisateur n'est enregistré</div>";
+} else {
+    $result .= "<div class='reponse'>Aucun Utilisateur n'est enregistré</div>";
 }
 
 // tableau pour le graphique
@@ -38,56 +38,56 @@ foreach ($nombreparmois as $cle) {
 $final = join(',', $tableau);
 
 
-if(!empty($meilleurRes) && !empty($meilleurSouv)){
-    if($meilleurRes[0]['total'] >= $meilleurSouv[0]['total']){
+if (!empty($meilleurRes) && !empty($meilleurSouv)) {
+    if ($meilleurRes[0]['total'] >= $meilleurSouv[0]['total']) {
         $meilleur = $meilleurRes[0]['Titre'];
-    }
-    else{
+    } else {
         $meilleur = $meilleurSouv[0]['nom'];
     }
-}
-else{
+} else {
     $meilleur = "aucun panier n'a encore été validé, il est alros impossible de définir une préférence.";
 }
 
 ?>
 
+<div class="MiseEnPage">
+    <div class="dashboard">
+        <div class="nombre">
+            <div class="nombrePanierValide">
+                <img src="../img/Shopping_bag.png" alt="une icone">
+                <div class="stat">
+                    Nombre de paniers validés : <?= $comptepanier ?>
+                </div>
+            </div>
+            <div class="nombreInscrit">
+                <img src="../img/Users.svg" alt="une icone">
+                <div class="stat">
+                    Nombre d'inscriptions : <?= $compteinscrit ?>
+                </div>
+            </div>
+            <!-- <div class="nombreConnexion">
+            <img src="" alt="une icone">
+            <div class="stat"> -->
 
-<div class="dashboard">
-    <div class="nombre">
-        <div class="nombrePanierValide">
-            <img src="" alt="une icone">
-            <div class="stat">
-                Nombre de paniers validés : <?= $comptepanier ?>
+            <!-- </div>
+        </div> -->
+            <div class="nombrePanierValide">
+                <img src="../img/Shopping_cart.png" alt="une icone">
+                <div class="stat">
+                    Produit préféré : <?= $meilleur ?>
+                </div>
             </div>
         </div>
-        <div class="nombreInscrit">
-            <img src="" alt="une icone">
-            <div class="stat">
-                Nombre d'inscriptions : <?= $compteinscrit ?>
-            </div>
-        </div>
-        <div class="nombreConnexion">
-            <img src="" alt="une icone">
-            <div class="stat">
-
-            </div>
-        </div>
-        <div class="nombrePanierValide">
-            <img src="" alt="une icone">
-            <div class="stat">
-                Produit préféré : <?= $meilleur ?>
+        <div class="MiseEnPage">
+            <div class="graph">
+                <canvas id="myChart"></canvas>
             </div>
         </div>
     </div>
-    <div class="graph">
-            <canvas id="myChart"></canvas>
+    <div class="usersAll">
+        <?= $result ?>
     </div>
 </div>
-<div class="usersAll">
-    <?= $result ?>
-</div>
-
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
     // grapique chart js

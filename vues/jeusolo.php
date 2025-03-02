1b93de723a2e319863d9d126c9d5907e66443d9d
Maxence_Sanchez <maxence.sanchez-varas-leclercq@uha.fr>
2025-03-02 19:06:44 +0100

    gv

--- vues/jeusolo.php
+++ vues/jeusolo.php
@@ -19,31 +19,34 @@ if (file_exists('img/photojeu/' . $_GET['idjeu'] . '.jpg')) {
 if ($jeu[0]['lien_video'] == "") {
     $video = "";
 } else {
-    $video = $jeu[0]['lien_video'];
+    $video = '<iframe width="560" height="315" src="'.$jeu[0]['lien_video'].'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
 }
 
 $paniers = new panier;
 
 $script = "<script src='../js/script_jeusolo.js'></script>";
-    ?>
+
+?>
 
 <div class="jeutop">
     <div class="image">
         <img src="<?= $phototest ?>" alt="photo_de_l'escape-game">
     </div>
     <div class="infos">
-        <div class="titre">
+        <div class="titrePage">
             <h1><?= $jeu[0]['Titre'] ?></h1>
+            <div class="rectangleTitre">
+            </div>
         </div>
         <div class="description">
             <?= $jeu[0]['description'] ?>
         </div>
         <div class="autre">
             <div class="age">
-                Age minimum : <?= $jeu[0]['age'] ?>
+                <p>Age minimum : <?= $jeu[0]['age'] ?></p>
             </div>
             <div class="nombre">
-                De <?= $jeu[0]['nombre_min'] ?> à <?= $jeu[0]['nombre_max'] ?> participants.es
+                <p>De <?= $jeu[0]['nombre_min'] ?> à <?= $jeu[0]['nombre_max'] ?> participants.es</p>
             </div>
         </div>
     </div>
@@ -53,17 +56,18 @@ $script = "<script src='../js/script_jeusolo.js'></script>";
 </div>
 <div class="fleches">
     <div class="gauche">
-        gauche
+        <img src="../img/LeftArrow.svg" alt="fleche gauche">
     </div>
     <div class="droite">
-        Droite
+        <img src="../img/RightArrow.svg" alt="fleche gauche">
     </div>
 </div>
 <div class="calendrier">
+    
 </div>
 
 <div class="cache">
     <!-- div cachée, sert juste à avoir le min et le max en js -->
-     <div class="min" id="<?= $jeu[0]['nombre_min'] ?>"></div>
-     <div class="max" id="<?= $jeu[0]['nombre_max'] ?>"></div>
+    <div class="min" id="<?= $jeu[0]['nombre_min'] ?>"></div>
+    <div class="max" id="<?= $jeu[0]['nombre_max'] ?>"></div>
 </div>

80155f7c56fcceb1835c90688bf0ad1cb7f020b7
Alexandre Spitzer <spitzeralexandre@gmail.com>
2025-03-03 17:17:25 +0100

    nous trouver (que je continue ptet (probablement pas) ce soir

--- vues/carte.php
+++ vues/carte.php
@@ -1,8 +1,54 @@
 <?php
 
-$styles="";
+$styles="../styles/style_carte.css";
 $librairie="";
 $script  = "";
 
 ?>
 
+<div class="lesCartes">
+    <h1>We-escape maintenant en france !</h1>
+    <div class="rectangleTitre"></div>
+    <div class="lesCartes">
+        <div class="france">
+            <img src="../img/france.svg" alt="">
+        </div>
+        <div class="germany">
+            <img src="../img/germany.svg" alt="">
+        </div>
+    </div>
+</div>
+<div class="lesVilles">
+<h2>Retrouvez nous dans ces villes !</h2>
+    <div class="rectangleTitre"></div>
+    <div class="DivVilles">
+        <div class="grandeVilles">Région grand est</div>
+        <div class="petiteVille">Mulhouse</div>
+        <div class="petiteVille">Strasbourg</div>
+    </div>
+    <div class="DivVilles">
+        <div class="grandeVilles">Lorraine</div>
+        <div class="petiteVille">Ville</div>
+        <div class="petiteVille">Ville</div>
+        <div class="petiteVille">Ville</div>
+        <div class="petiteVille">Ville</div>
+    </div>
+    <div class="DivVilles">
+        <div class="grandeVilles">Lorraine</div>
+        <div class="petiteVille">Ville</div>
+        <div class="petiteVille">Ville</div>
+        <div class="petiteVille">Ville</div>
+        <div class="petiteVille">Ville</div>
+    </div>
+    <div class="DivVilles">
+        <div class="grandeVilles">Lorraine</div>
+        <div class="petiteVille">Ville</div>
+        <div class="petiteVille">Ville</div>
+    </div>
+    <div class="DivVilles">
+        <div class="grandeVilles">Lorraine</div>
+        <div class="petiteVille">Ville</div>
+        <div class="petiteVille">Ville</div>
+        <div class="petiteVille">Ville</div>
+    </div>
+</div>

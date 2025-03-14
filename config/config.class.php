d1da0a4e85ae0974b1b2a695849cf8ff9f96d8b9
Maxence_Sanchez <maxence.sanchez-varas-leclercq@uha.fr>
2025-03-14 14:20:10 +0100

    LA FIN
    FINITOPIPO

--- config/config.class.php
+++ config/config.class.php
@@ -3,10 +3,14 @@
 // configuration a changer poru l'instalation de la bdd
 
 class Config{
-     public $DBHOST = "localhost";
-     public $DBNAME = "weescape";
-     public $DBUSER = "root";
-     public $DBPWD = "";
+     // public $DBHOST = "localhost";
+     // public $DBNAME = "weescape";
+     // public $DBUSER = "root";
+     // public $DBPWD = "";
 
+     public $DBHOST = "iutmmmisql03.uha.fr";
+     public $DBNAME = "e2303975";
+     public $DBUSER = "e2303975";
+     public $DBPWD = "989696";
      public $TITREONGLET = "We escape";
 }
